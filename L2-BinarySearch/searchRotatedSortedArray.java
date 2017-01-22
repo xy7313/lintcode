5690f65969cbe139d72461688acaf3776e10f853
xy7313 <syrupyang@hotmail.com>
2017-01-22 10:16:33 -0500

    search

--- L2-BinarySearch/searchRotatedSortedArray.java
+++ L2-BinarySearch/searchRotatedSortedArray.java
@@ -36,3 +36,45 @@ public class Solution {
         return -1;
     }
 }
+
+//这种感觉跟上面的好像啊。。
+public class Solution {
+    /** 
+     *@param A : an integer rotated sorted array
+     *@param target :  an integer to be searched
+     *return : an integer
+     */
+    public int search(int[] A, int target) {
+        if(A==null||A.length==0) return -1;
+        int start = 0;
+        int end = A.length-1;
+        while(start+1<end){
+            int mid = start+(end-start)/2;
+            if (A[mid] == target) {
+                return mid;
+            }
+            if (A[start] < A[mid]) {
+                // situation 1, red line
+                if (A[start] <= target && target <= A[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                // situation 2, green line
+                if (A[mid] <= target && target <= A[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        if (A[start] == target) {
+            return start;
+        }
+         if (A[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+}
