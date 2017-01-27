e2eac7c678fd726c5bf9ba3d4ebf14ee37311a49
xy7313 <syrupyang@hotmail.com>
2017-01-26 20:50:32 -0500

    2d

---
+++ L2-BinarySearch/search2Dmatrix.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /**
+     * @param matrix, a list of lists of integers
+     * @param target, an integer
+     * @return a boolean, indicate whether matrix contains target
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int start = 0, end = row * column - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            int number = matrix[mid / column][mid % column];
+            if (number == target) {
+                return true;
+            } else if (number < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (matrix[start / column][start % column] == target) {
+            return true;
+        } else if (matrix[end / column][end % column] == target) {
+            return true;
+        }
+        
+        return false;
+    }
+}
