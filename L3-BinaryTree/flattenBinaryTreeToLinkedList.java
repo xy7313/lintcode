c689d0e804224f1adee2cecb25ec0a08add2e44a
xy7313 <syrupyang@hotmail.com>
2017-02-02 19:02:02 -0500

    flatten more

--- L3-BinaryTree/flattenBinaryTreeToLinkedList.java
+++ L3-BinaryTree/flattenBinaryTreeToLinkedList.java
@@ -9,6 +9,8 @@
  *     }
  * }
  */
+
+ //iteration
 public class Solution {
     /**
      * @param root: a TreeNode, the root of the binary tree
@@ -35,5 +37,57 @@ public class Solution {
             }
         }
         
+    }
+}
+
+//traverse
+public class Solution {
+    private TreeNode lastNode;
+    public void flatten(TreeNode root) {
+        if(root==null) return;
+        
+        if(lastNode !=null){
+            lastNode.left = null;
+            lastNode.right = root;
+        }
+        
+        lastNode = root;
+        TreeNode right = root.right;
+        flatten(root.left);
+        flatten(right);
+    }
+}
+
+//divide-conquer
+public class Solution {
+     public void flatten(TreeNode root) {
+        helper(root);
+    }
+    
+    // flatten root and return the last node
+    private TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode leftLast = helper(root.left);
+        TreeNode rightLast = helper(root.right);
+        
+        // connect leftLast to root.right. similiar like insert root.left between root and root.right.
+        if (leftLast != null) {
+            leftLast.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        //if rightLast exists, it is the lastnode, else if leftLast exists, it is the lastnode, else, root is the lastnode
+        if (rightLast != null) {
+            return rightLast;
+        }
+        
+        if (leftLast != null) {
+            return leftLast;
+        }
+        
+        return root;
     }
 }
