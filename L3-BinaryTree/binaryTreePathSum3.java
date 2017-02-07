62119f600d0e4d725e3689571f3c53df8465dd4d
xy7313 <syrupyang@hotmail.com>
2017-02-07 18:23:44 -0500

    path3

---
+++ L3-BinaryTree/binaryTreePathSum3.java
@@ -0,0 +1,56 @@
+/**
+ * Definition of ParentTreeNode:
+ * 
+ * class ParentTreeNode {
+ *     public int val;
+ *     public ParentTreeNode parent, left, right;
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @param target an integer
+     * @return all valid paths
+     */
+    public List<List<Integer>> binaryTreePathSum3(ParentTreeNode root, int target) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        dfs(root, target, results);
+        return results;
+    }
+    
+    public void dfs(ParentTreeNode root, int target, List<List<Integer>> results) {
+        if (root == null)
+            return;
+
+        List<Integer> path = new ArrayList<Integer>();
+        findSum(root, null, target, path, results);
+
+        dfs(root.left, target, results);
+        dfs(root.right, target, results);
+    }
+
+    public void findSum(ParentTreeNode root, ParentTreeNode father, int target,
+                 List<Integer> path, List<List<Integer>> results) {
+        path.add(root.val);
+        target -= root.val;
+        
+        if (target == 0) {
+            // ArrayList<Integer> tmp = new ArrayList<Integer>();
+            // Collections.addAll(tmp,  new  Integer[path.size()]); 
+            // Collections.copy(tmp, path); 
+            // results.add(tmp);
+            results.add(new ArrayList<Integer>(path));
+        }
+
+        if (root.parent != null && root.parent != father)
+            findSum(root.parent, root, target, path, results);
+
+        if (root.left != null && root.left  != father)
+            findSum(root.left, root, target, path, results);
+
+        if (root.right != null && root.right != father)
+            findSum(root.right, root, target, path, results);
+
+        path.remove(path.size() - 1);
+    }
+}
